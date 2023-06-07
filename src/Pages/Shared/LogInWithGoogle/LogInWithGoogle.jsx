584354a594476ff2585b0bbd7e7bb95db1874ce7
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-06-07 20:43:02 +0600

    implemented login with google function

--- src/Pages/Shared/LogInWithGoogle/LogInWithGoogle.jsx
+++ src/Pages/Shared/LogInWithGoogle/LogInWithGoogle.jsx
@@ -1,9 +1,19 @@
+import { useAuth } from "../../../Hooks/useAuth";
+
 const LogInWithGoogle = () => {
+  const { googleLogin, user } = useAuth();
+  console.log(user);
+  const handleGoogleLogin = () => {
+    googleLogin()
+      .then(res => console.log(res.user))
+      .catch(err => console.log(err));
+  };
   return (
     <div className="flex justify-center max-w-sm mx-auto">
       <button
         className="btn btn-ghost normal-case flex w-full"
         style={{ border: "2px solid lightgray" }}
+        onClick={handleGoogleLogin}
       >
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png"
--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,18 +1,41 @@
-import { createContext, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import app from "../../Utility/Firebase/Firebase.config";
 
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
+  const googleProvider = new GoogleAuthProvider();
   const auth = getAuth(app);
   const [user, setUser] = useState(null);
+
+  //create user with email and pass
   const createAccount = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  //log in with google
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //observe user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, [auth]);
   const data = {
-    name: "Md. Abir mahmud",
     createAccount,
+    googleLogin,
     user,
   };
   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
