44172ff41518b21d3904d036287820dfec03dc36
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-06-08 08:31:01 +0600

    implemented instructor page

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,8 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAxiosSecure } from "../../Hooks/useAxiosSecure";
+
 const Instructors = () => {
+  const [axiosSecure] = useAxiosSecure();
+  const { data: instructors = [] } = useQuery(["instructors"], async () => {
+    const res = await axiosSecure.get("/instructor");
+    return res.data;
+  });
+  console.log(instructors);
   return (
-    <div>
-      <h2>from Instructors</h2>
-    </div>
+    <section>
+      <div className="cs-container">
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr className="text-base">
+                <th></th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody className="text-base">
+              {instructors.map((instructor, index) => (
+                <tr key={instructor._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <img
+                      src={instructor.image}
+                      alt={instructor.name}
+                      className="w-16 h-16 rounded-full"
+                    />
+                  </td>
+                  <td>{instructor.name}</td>
+                  <td>{instructor.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   );
 };
 
